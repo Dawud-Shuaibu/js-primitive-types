3c660f3de91cb83eee8db967079fdc13062adcf9
Dawud-Shuaibu <dawudshuaibu6565@gmail.com>
2024-10-23 14:01:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// JavaScript Primitive or Value Types
+
+let name = 'John'; // String
+let age = 20; // Number
+let isApproved = true; // Boolean
+let lastName = undefined; // undefined
+let selectedColor = null; // null
+
+console.log(name);
+console.log(age);
+console.log(isApproved);
+console.log(lastName);
+console.log(selectedColor);
+
+/* Cannot be a reserved Keyword
+Cannot start with a number e.g(1name)
+Cannot contain space or hyphen e.g(-)
+Should be meaningful
+Are case sensitive */
